ff265bc80ab73a21904c4b7a44b1c14e4c71ab8e
drishtyjha1 <45451398+drishtyjha1@users.noreply.github.com>
2020-11-02 00:00:14 +0530

    Add navigationView

--- app/src/main/java/com/example/rago2/MainActivity.java
+++ app/src/main/java/com/example/rago2/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.rago2;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +29,44 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox mChRememberMe;
 
 
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       toolbar=(Toolbar)findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+       nav=(NavigationView)findViewById(R.id.nav_view);
+       drawerLayout =(DrawerLayout)findViewById(R.id.drawer);
+       toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+       drawerLayout.addDrawerListener(toggle);
+       toggle.syncState();
+
+       nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+               switch (item.getItemId()){
+                   case R.id.menu_home:
+                       Toast.makeText(getApplicationContext(),"Home panel is open",Toast.LENGTH_LONG).show();
+                       drawerLayout.closeDrawer(GravityCompat.START);
+                       break;
+
+                   case R.id.menu_edit:
+                       Toast.makeText(getApplicationContext(),"Edit panel is open",Toast.LENGTH_LONG).show();
+                       drawerLayout.closeDrawer(GravityCompat.START);
+                       break;
+
+               }
+               return true;
+           }
+       });
 
         mEtPassword = findViewById(R.id.etPassword);
         etenterEmail = findViewById(R.id.editTextTextEmailAddress);
